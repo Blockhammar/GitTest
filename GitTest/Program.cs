4b7e1051af47e503f64e89dc6917ea4974c98227
Sebastian Engh <enghseb@gmail.com>
2022-11-28 15:15:17 +0100

    Ny bil

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, Group!");
             var Car = new Car("Honda","Civic");
             var Person = new Person("Alex", 25);
+			var Car2 = new Car("Skoda", "Fabia");
         }
     }
 }
