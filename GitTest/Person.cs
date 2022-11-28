4a484650f93d4db1366073f1938c87c077ac7054
Martin Billberg <martinmidas@gmail.com>
2022-11-28 15:12:20 +0100

    Lagt till name och age const

--- GitTest/Person.cs
+++ GitTest/Person.cs
@@ -8,7 +8,13 @@ namespace GitTest
 {
 	internal class Person
 	{
-		public string Name { get; set; }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
 		public int Age { get; set; }
 	}
 }
